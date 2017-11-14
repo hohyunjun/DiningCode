a80cf01aab494d99a72c6394f8a9d0e3a3908c91
DESKTOP-G7MQ1D1\jhh51 <jh5154@kookmin.ac.kr>
2017-11-14 22:34:11 +0900

    선택지 UI 디자인 추가

--- MyApplication/app/src/main/java/com/example/one10/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/one10/myapplication/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.one10.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-        Button btn=(Button) findViewById(R.id.button_what);
+        Button btn= findViewById(R.id.button_what);
         btn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent intent=new Intent(getApplicationContext(),Select_main.class);
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btn2=(Button) findViewById(R.id.button_search);
+        Button btn2= findViewById(R.id.button_search);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,5 +63,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 }

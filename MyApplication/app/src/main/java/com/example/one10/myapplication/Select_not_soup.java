d273f3faa98fcb818fde06b8580d10d6da9854f9
DESKTOP-G7MQ1D1\jhh51 <jh5154@kookmin.ac.kr>
2017-11-15 02:05:33 +0900

    최종 ListView 틀 제작 및 인스턴스 추가(인스턴스 수정 필요)

--- MyApplication/app/src/main/java/com/example/one10/myapplication/Select_not_soup.java
+++ MyApplication/app/src/main/java/com/example/one10/myapplication/Select_not_soup.java
@@ -2,6 +2,10 @@ package com.example.one10.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by one10 on 2017-11-01.
@@ -12,5 +16,15 @@ public class Select_not_soup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_not_soup);
+
+        ListView listview=(ListView)findViewById(R.id.listview5); // 리스트뷰 객체화
+        ArrayList<String> data = new ArrayList<String>(); // 리스트뷰에 담을 배열
+        data.add("송백식당");
+        data.add("쌈스토리");
+        data.add("그림비");
+        data.add("두번째이야기");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+
+        listview.setAdapter(adapter);
     }
 }
--- MyApplication/app/src/main/java/com/example/one10/myapplication/Select_snack.java
+++ MyApplication/app/src/main/java/com/example/one10/myapplication/Select_snack.java
@@ -2,6 +2,10 @@ package com.example.one10.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by one10 on 2017-11-01.
@@ -12,5 +16,18 @@ public class Select_snack extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_snack);
+        ListView listview=(ListView)findViewById(R.id.listview6); // 리스트뷰 객체화
+        ArrayList<String> data = new ArrayList<String>(); // 리스트뷰에 담을 배열
+        data.add("김밥고을");
+        data.add("엽기떡볶이");
+        data.add("빙수야");
+        data.add("불난집");
+        data.add("전설의냄비떡볶이");
+        data.add("신전떡볶이");
+        data.add("국민상회");
+        data.add("맛사랑");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+
+        listview.setAdapter(adapter);
     }
 }

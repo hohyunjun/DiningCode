a53c7c5be01cbecb5cdd5583717238d0280fffde
DESKTOP-8DP0IN2\one10 <ryumhoo123@kookmin.ac.kr>
2017-11-27 13:28:39 +0900

    re

--- MyApplication/app/src/main/java/com/example/one10/myapplication/Select_china.java
+++ MyApplication/app/src/main/java/com/example/one10/myapplication/Select_china.java
@@ -2,6 +2,10 @@ package com.example.one10.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by one10 on 2017-11-01.
@@ -12,5 +16,19 @@ public class Select_china extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_china);
+
+        ListView listview=(ListView)findViewById(R.id.listview2); // 리스트뷰 객체화
+        ArrayList<String> data = new ArrayList<String>(); // 리스트뷰에 담을 배열
+        data.add("삼성원");
+        data.add("보성각");
+        data.add("임금님");
+        data.add("콰이러");
+        data.add("북악루");
+        data.add("주이마라");
+        data.add("훠거집");
+        data.add("청록원");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+
+        listview.setAdapter(adapter);
     }
 }
--- MyApplication/app/src/main/java/com/example/one10/myapplication/Select_not_chicken.java
+++ MyApplication/app/src/main/java/com/example/one10/myapplication/Select_not_chicken.java
@@ -2,6 +2,10 @@ package com.example.one10.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by one10 on 2017-11-01.
@@ -11,8 +15,6 @@ public class Select_not_chicken extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_not_chicken);
-<<<<<<< HEAD
-=======
 
         ListView listview=(ListView)findViewById(R.id.listview1); // 리스트뷰 객체화
         ArrayList<String> data = new ArrayList<String>(); // 리스트뷰에 담을 배열
@@ -27,6 +29,5 @@ public class Select_not_chicken extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
 
         listview.setAdapter(adapter);
->>>>>>> ce20b9b17a4dfedadb9f4193c5ea8f04a7f5d736
     }
 }
